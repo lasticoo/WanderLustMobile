f1837ea389acd91357dffde4ca5769de0d442287
Lasticoo <lastico.alparesz@gmail.com>
2024-09-27 20:48:06 +0700

    update:login,dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.circleimageview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-}
+    implementation(libs.google.material)
+    }
