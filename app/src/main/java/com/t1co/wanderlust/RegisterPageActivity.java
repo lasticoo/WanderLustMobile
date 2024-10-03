eff06c49350e9c0a6737891deade931de8ffd09d
syausanvalia <inigopaal@gmail.com>
2024-10-03 15:10:29 +0700

    commit

--- app/src/main/java/com/t1co/wanderlust/RegisterPageActivity.java
+++ app/src/main/java/com/t1co/wanderlust/RegisterPageActivity.java
@@ -20,12 +20,15 @@ public class RegisterPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        Button button_register = findViewById(R.id.button_register);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterPageActivity.this, LoginPageActivity.class);
+                startActivity(intent);
+            }
+        });
+        }
     }
-}
+
