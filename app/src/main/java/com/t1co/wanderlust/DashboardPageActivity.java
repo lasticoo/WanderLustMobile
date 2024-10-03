1a414b5d25445e9dff9f6cab5ffe4f844fb24744
Lasticoo <lastico.alparesz@gmail.com>
2024-10-03 14:02:27 +0700

    update:login:dashboard

--- app/src/main/java/com/t1co/wanderlust/DashboardPageActivity.java
+++ app/src/main/java/com/t1co/wanderlust/DashboardPageActivity.java
@@ -2,12 +2,8 @@ package com.t1co.wanderlust;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.widget.ViewFlipper;
 
 public class DashboardPageActivity extends AppCompatActivity {
@@ -17,31 +13,25 @@ public class DashboardPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard_page);
 
+        // Initialize the ViewFlipper
         viewFlipper = findViewById(R.id.viewFlipper);
+        viewFlipper.setFlipInterval(1750);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.startFlipping(); // Start the flipping
 
-        // Set interval untuk flip otomatis (dalam milidetik)
-        viewFlipper.setFlipInterval(1750); // Setiap 1.75 detik
-        viewFlipper.setAutoStart(true); // Mulai flip otomatis
-        viewFlipper.startFlipping();
-
-        // Load default fragment
-    }
-
-
-
+    } // Closing brace for onCreate
 
     @Override
     protected void onPause() {
         super.onPause();
-        viewFlipper.stopFlipping(); // Hentikan flip saat aktivitas tidak aktif
+        viewFlipper.stopFlipping(); // Stop flipping when paused
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        viewFlipper.startFlipping(); // Mulai flip lagi saat aktivitas dilanjutkan
+        viewFlipper.startFlipping(); // Resume flipping when resumed
     }
 }
---
+++ app/src/main/java/com/t1co/wanderlust/ProfilePageActivity.java
@@ -0,0 +1,15 @@
+package com.t1co.wanderlust;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+
+    }
